9bfbbf667c3f4a91752712cbf884633dd0a91922
gabimoisa1998 <gabriel.moisa@opswat.com>
2023-10-12 13:29:58 +0300

    MDCL-7343 WIP

--- src/pages/about/About.test.js
+++ src/pages/about/About.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import BackgroundContext from '../../providers/Background';
+import BackgroundContext from '../../providers/UserProvider';
 import About from './About';
 
 describe('About', () => {
