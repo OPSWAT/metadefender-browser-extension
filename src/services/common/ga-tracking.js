b314d95e63aaf8c7ecde7e457306e43272d5b62b
gabimoisa1998 <gabriel.moisa@opswat.com>
2024-01-10 11:59:46 +0200

    MDCL-7679 Fix Manifest V3

--- src/services/common/ga-tracking.js
+++ src/services/common/ga-tracking.js
@@ -14,7 +14,7 @@
         const ga = document.createElement('script');
         ga.type = 'text/javascript';
         ga.async = true;
-        ga.src = 'https://ssl.google-analytics.com/ga.js';
+        ga.src = require('./ga');
         const s = document.getElementsByTagName('script')[0];
         s.parentNode.insertBefore(ga, s);
     }
