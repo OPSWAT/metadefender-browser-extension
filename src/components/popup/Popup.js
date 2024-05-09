9539e812f5a0514b8d6d246b74f27e4e4b5ac49b
b.ichim <b.ichim@italiamultimedia.com>
2024-05-09 16:09:26 +0300

    feat_bogdan_dima: added front-end to popup.

--- src/components/popup/Popup.js
+++ src/components/popup/Popup.js
@@ -1,6 +1,6 @@
 
 import classNames from 'classnames';
-import React, { useEffect, useMemo, useContext } from 'react';
+import React, { useEffect, useMemo, useContext, useState } from 'react';
 import GAContext from '../../providers/GAProvider';
 import ConfigContext from '../../providers/ConfigProvider';
 import { goToTab } from '../../services/background/navigation';
@@ -15,6 +15,8 @@ const Popup = () => {
     const { gaTrackEvent } = useContext(GAContext);
     const { files } = useContext(ScanHistoryContext);
     const scanUrl = config.mclDomain;
+    const [dropOverlayActive, setDropOverlayActive] = useState(false);
+
 
     /**
      * Send google analytics data on click event
@@ -44,25 +46,67 @@ const Popup = () => {
         return 'icon-help';
     };
 
-    /** Open a new tab with the full scan history */
+    const getRelativeScanTime = (timestamp) => {
+        const timeDifference = (Math.floor(Date.now() / 1000) - timestamp);
+
+        const intervals = [
+            {label: 'year', seconds: 31536000 },
+            {label: 'month', seconds: 2592000},
+            {label: 'day', seconds: 86400},
+            {label: 'hour', seconds: 3600},
+            {label: 'minute', seconds: 60},
+            {label: 'second', seconds: 1}
+        ]
+
+        for (let i = 0; i < intervals.length; i++) {
+            const count = Math.floor(timeDifference / intervals[i].seconds);
+            if(count >= 1) {
+                return `${count} ${intervals[i].label}${count !== 1 ? 's' : ''} ago`;
+
+            }
+        }
+
+        return 'just now';
+    };
+
     const goToHistory = () => {
         handleGaTrack();
         goToTab('history');
         window.close();
     };
 
-    /** Open a new tab with the settings */
     const goToSettings = () => {
         handleGaTrack();
         goToTab('settings');
         window.close();
     };
 
+    const dropOverlay = useMemo(() => {
+        return (
+            <div className="drop-overlay"></div>
+        );
+    }, [dropOverlayActive]);
+
+    const handleDragAndDrop = (event) => {
+        event.preventDefault();
+        event.stopPropagation(); 
+        console.log(event.type);
+        if (event.type === 'dragover') {
+            setDropOverlayActive(true);
+        } else if (event.type === 'drop') {
+            const file = event.dataTransfer.files[0];
+            setDropOverlayActive(false);
+            console.log(file);
+        }
+    };
+
+    
+
     useEffect(() => {
         gaTrackEvent(['_trackPageview', '/extension/popup']);
     }, []);
 
-    const viewScanHistoryClassName = classNames({ 'd-none': files.length === 0 }, 'popup--scan__footer text-right');
+    const viewScanHistoryClassName = classNames({ 'd-none': files.length === 0 }, 'popup--scan__footer');
 
     const getScanUrl = (file) => {
         if (file.dataId) {
@@ -76,15 +120,35 @@ const Popup = () => {
         if (files.length === 0) {
             return;
         }
+        
+        
+        const tableRows = files.slice(0, 3).map((scannedFile, index) => {
+            console.log(scannedFile);
+            return (
+            <tr key={index} className="list-group-item d-flex align-items-center justify-content-between">
+                <td>
+                        <a href={scannedFile.scanResults || getScanUrl(scannedFile)} target="_blank" rel="noreferrer noopener">
+                            {scannedFile.fileName}
+                        </a>
+                        <span>
+                            {scannedFile.dataId}
+                        </span>
+                </td>
+                <td>
+                    {getRelativeScanTime(scannedFile.scanTime)}
+                </td>
+                <td>
+                    <span className={`mcl-icon ${getStatusIcon(scannedFile.status)}`}></span>
+                </td>
+            </tr>
+            );
+        });
 
-        return files.slice(0, 3).map((scannedFile, index) => (
-            <li key={index} className="list-group-item d-flex align-items-center justify-content-between">
-                <a href={scannedFile.scanResults || getScanUrl(scannedFile)} target="_blank" rel="noreferrer noopener">
-                    {scannedFile.fileName}
-                </a>
-                <span className={`mcl-icon ${getStatusIcon(scannedFile.status)}`}></span>
-            </li>
-        ));
+        return (
+            <tbody>
+                {tableRows}
+            </tbody>
+        )
     }, [files]);
 
     const scanResults = useMemo(() => {
@@ -96,28 +160,47 @@ const Popup = () => {
             </ul>;
         }
 
-        return <ul className="list-group row">
+        return <table className="list-group row">
+                    <thead>
+                        <tr>
+                            <td>FILE NAME</td>
+                            <td>SCAN TIME</td>
+                            <td>RESULT</td>
+                        </tr>
+                    </thead>
+                    
             {scanResultsDom}
-        </ul>;
+            
+        </table>;
     }, [files, scanResultsDom]);
 
-    return <div className="popup--wrapper">
+    const dropFile = useMemo(() => {
+        return <div className="dnd-bar" >
+                    <i className="icon-drop"></i>
+                    <span className="text" dangerouslySetInnerHTML={{ __html: chrome.i18n.getMessage('dropFile') }} />
+            </div>;
+    })
+
+    return <div className="popup--wrapper" onDrop={handleDragAndDrop} onDragOver={handleDragAndDrop}>
+    <div className={`drop-overlay ${dropOverlayActive ? 'active' : ''}`}></div>
         <div className="popup--header">
             <div className="popup--header__logo"></div>
-            <a href='#' className="popup--header__btn" onClick={goToSettings}>
-                <span className="icon-cog text-14"></span>
-            </a>
+                <div className="popup--header__buttons">
+                    <a href='#' className={classNames("popup--header__btn", viewScanHistoryClassName)} onClick={goToHistory}>
+                        <span className="icon-history text-14"></span>
+                    </a>
+                    <a href='#' className="popup--header__btn" onClick={goToSettings}>
+                        <span className="icon-cog text-14"></span>
+                    </a>
+                </div>
         </div>
 
         <div className="popup--scan__history">
             {scanResults}
         </div>
 
-        <div className={viewScanHistoryClassName}>
-            <a href="#" onClick={goToHistory}>
-                View Scan History
-                <span className="mcl-icon icon-right"></span>
-            </a>
+        <div className="popup--drop__file text-right">
+            {dropFile}
         </div>
     </div>;
 };
