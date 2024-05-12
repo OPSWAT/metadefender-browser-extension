8a8a81986bccca59fb04f2bb207f5c17d7cd1509
Denisa <denisa.dinesc03@e-uvt.ro>
2024-05-12 13:51:40 +0300

    no domains message

--- src/pages/domain-reputation/DomainReputation.js
+++ src/pages/domain-reputation/DomainReputation.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useContext, useEffect, useMemo, useState } from 'react';
+import { Button, Col, Form, InputGroup, Row } from 'react-bootstrap';
+
+import SidebarLayout from '../../components/common/sidebar-layout/SidebarLayout';
+import ScanHistoryTable from '../../components/scan-history-table/ScanHistoryTable';
+import ScanFile from '../../services/common/scan-file';
+
+import ConfigContext from '../../providers/ConfigProvider';
+import GAContext from '../../providers/GAProvider';
+// import ScanHistoryContext from '../../providers/ScanHistoryProvider';
+
+import './DomainReputation.scss';
+
 
 const DomainReputation = () => {
-  return (
-    <div>DomainReputation</div>
-  )
+    const content = 
+    <Row>
+        <Col className='mt-4 text-center font-weight-bold'>
+            <span dangerouslySetInnerHTML={{ __html: chrome.i18n.getMessage('noDomainsNotification') }} />
+        </Col>
+    </Row>
+    
+  return <SidebarLayout
+        className='domain'
+        currentPage='domain'
+        content={content}
+    />;
 }
 
 export default DomainReputation
