effb7f1ab3a7fe02cabcda994b019ea903ba87a2
Denisa <denisa.dinesc03@e-uvt.ro>
2024-05-15 22:44:19 +0300

    api response in pup-up

--- src/components/popup/utils/popup_domain.js
+++ src/components/popup/utils/popup_domain.js
@@ -2,6 +2,14 @@ import MetascanClient from "../../../services/common/metascan-client";
 import { apikeyInfo } from "../../../services/common/persistent/apikey-info";
 
 export const sendDomainToApi = async () => {
+    function getTrustworthySources(apiResponse) {
+        if (apiResponse && apiResponse.lookup_results && apiResponse.lookup_results.sources) {
+          return apiResponse.lookup_results.sources.filter(source => source.assessment !== '');
+        } else {
+          return [];
+        }
+    };
+
     return new Promise(async (resolve, reject) => {
         chrome.tabs.query({active: true, currentWindow: true}, async function(tabs) {
             const tab = tabs[0];
@@ -17,7 +25,9 @@ export const sendDomainToApi = async () => {
             try {
                 const response = await MetascanClient.setAuth(apikeyInfo.data.apikey)?.domain?.lookup(domain);
                 console.log(response.lookup_results.sources);
-                resolve(response);
+                const trustworthySources = getTrustworthySources(response);
+                console.log(trustworthySources)
+                resolve(trustworthySources);
             } catch (error) {
                 console.log('err', error);
                 reject(error);
