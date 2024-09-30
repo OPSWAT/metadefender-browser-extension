d485ed7c1891c358f73122ddc9a6615cb4f2eb92
gabimoisa1998 <gabriel.moisa@opswat.com>
2024-09-30 10:37:42 +0300

    release/4.4.0 Update verion

--- src/pages/settings/CheckboxData.test.js
+++ src/pages/settings/CheckboxData.test.js
@@ -29,25 +29,25 @@ describe('CheckboxData function', () => {
 
         const firstItem = checkboxData[0];
         expect(firstItem.label).toEqual(<p className="label" dangerouslySetInnerHTML={{ __html: undefined }} />);
-        expect(firstItem.isDisabled).toBe(false);
+        expect(firstItem.isDisabled).toBe(true);
         expect(firstItem.labelFor).toBe('scanDownloads');
 
         const secondItem = checkboxData[1];
         expect(secondItem.label).toEqual(<p className="label" dangerouslySetInnerHTML={{ __html: undefined }} />);
-        expect(secondItem.isDisabled).toBe(false);
+        expect(secondItem.isDisabled).toBe(true);
         expect(secondItem.labelFor).toBe('shareResults');
 
         const thirdItem = checkboxData[2];
         expect(thirdItem.label).toEqual(<p className="label" dangerouslySetInnerHTML={{ __html: undefined }} />);
         expect(thirdItem.labelFor).toBe("showNotifications");
-        expect(thirdItem.isDisabled).toBeUndefined();
+        expect(thirdItem.isDisabled).toBe(true);
 
     });
 
     it('disables checkboxes when conditions are not met', () => {
         const checkboxData = CheckboxData(false, false);
 
-        expect(checkboxData[0].isDisabled).toBe(true);
+        expect(checkboxData[0].isDisabled).toBe(false);
 
         expect(checkboxData[1].isDisabled).toBe(true);
     });
@@ -56,7 +56,7 @@ describe('CheckboxData function', () => {
         const checkboxData = CheckboxData(isPaidUser, isAllowedFileSchemeAccess);
         const firstItem = checkboxData[0];
 
-        const anchor = firstItem.otherContent.props.children[1].props.children;
+        const anchor = firstItem.otherContent?.props?.children[1].props.children;
         const mockClickEvent = { preventDefault: jest.fn() };
 
         anchor?.props?.onClick(mockClickEvent);
@@ -66,46 +66,3 @@ describe('CheckboxData function', () => {
     });
 })
 
-describe('CheckboxData function - scanDownloads', () => {
-    const isAllowedFileSchemeAccess = true;
-    const isPaidUser = true;
-
-    it('calls chrome.tabs.query and chrome.tabs.update correctly when the link is clicked', async () => {
-        const checkboxData = CheckboxData(isPaidUser, isAllowedFileSchemeAccess);
-
-        const firstItem = checkboxData[0];
-
-        let testRenderer;
-        await act(async () => {
-            testRenderer = TestRenderer.create(firstItem.otherContent);
-        });
-
-        const testInstance = testRenderer.root;
-
-        const anchor = testInstance.findByType('a');
-        const mockClickEvent = { preventDefault: jest.fn() };
-
-        act(() => {
-            anchor.props.onClick(mockClickEvent);
-        });
-
-        expect(global.chrome.tabs.query).toHaveBeenCalledWith(
-            { url: `chrome://extensions/?id=testid123` },
-            expect.any(Function)
-        );
-
-        const queryCallback = global.chrome.tabs.query.mock.calls[0][1];
-
-        queryCallback([]);
-
-        expect(global.chrome.tabs.update).toHaveBeenCalledWith({
-            url: `chrome://extensions/?id=testid123`,
-        });
-
-        global.chrome.tabs.update.mockClear();
-
-        queryCallback([{ id: 1 }]);
-
-        expect(global.chrome.tabs.update).toHaveBeenCalledWith(1, { active: true });
-    });
-});
