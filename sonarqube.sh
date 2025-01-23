0c7bfa899dbb0cec32676fda705ae5a07b14ab4c
Andrei Brindas <andrei.brindas@opswat.com>
2025-01-23 11:20:02 +0200

    added sonar script

---
+++ sonarqube.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SONARQUBE_URL=${SONARQUBE_URL-"https://sonar.opswat.com"}
+PROJECT_VERSION=${PROJECT_VERSION-${VERSION}}
+PR_ID=$1
+SOURCE=$2
+TARGET=$3
+
+docker run \
+    --rm \
+    --add-host=sonar.opswat.com:10.192.9.121 \
+    -e SONAR_HOST_URL="${SONARQUBE_URL}" \
+    -e SONAR_TOKEN="${SONARQUBE_TOKEN}" \
+    -v "$(pwd):/usr/src" \
+    sonarsource/sonar-scanner-cli:latest \
+    -Dsonar.pullrequest.key="$PR_ID" \
+    -Dsonar.pullrequest.branch="$SOURCE" \
+    -Dsonar.pullrequest.base="$TARGET"
