ceb62319e0d7f779a51aed7fc12ba11ea786f546
Andrei Brindas <andrei.brindas@opswat.com>
2025-01-23 14:47:55 +0200

    test sonar

--- sonarqube.sh
+++ sonarqube.sh
@@ -11,7 +11,7 @@ docker run \
     --add-host=sonar.opswat.com:10.192.9.121 \
     -e SONAR_HOST_URL="${SONARQUBE_URL}" \
     -e SONAR_TOKEN="${SONARQUBE_TOKEN}" \
-    -v "$(pwd):/usr/src" \
+    -v "$(pwd):/src" \
     sonarsource/sonar-scanner-cli:latest \
     -Dsonar.pullrequest.key="$PR_ID" \
     -Dsonar.pullrequest.branch="$SOURCE" \
